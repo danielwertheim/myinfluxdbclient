f8dacd8543bf57e305ee29af1fd8f3f1b768d170
Daniel Wertheim <daniel@wertheim.se>
2015-11-28 23:06:45 +0100

    UnitTest resolutions

---
+++ src/tests/MyInfluxDbClient.UnitTests/MillisecondsResolutionTests.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace MyInfluxDbClient.UnitTests
+{
+    public class MillisecondsResolutionTests : UnitTestsOf<MillisecondsResolution>
+    {
+        public MillisecondsResolutionTests()
+        {
+            SUT = new MillisecondsResolution();
+        }
+
+        [Test]
+        public void NanosecondsFrom_Should_return_nanoseconds_by_multiplying_100_to_reminders_of_ticks_subtracted_by_modulus_10000()
+        {
+            var ts = TimeStamp.Now();
+
+            var ns = SUT.NanosecondsFrom(ts);
+
+            ns.Should().Be((ts.Ticks - ts.Ticks % 10000) * 100);
+        }
+
+        [Test]
+        public void NanosecondsFrom_Should_return_value_with_six_ending_zeroes()
+        {
+            var dt = new DateTime(635843473895094535);
+            var ts = TimeStamp.From(dt);
+
+            var ns = SUT.NanosecondsFrom(ts);
+
+            var s = string.Join(string.Empty, ns.ToString().Reverse());
+            s.Should().StartWith("000000");
+            s.Skip(6).First().Should().NotBe('0');
+        }
+    }
+}
---
+++ src/tests/MyInfluxDbClient.UnitTests/UnixDateTime.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyInfluxDbClient.UnitTests
+{
+    internal static class UnixDateTime
+    {
+        internal static DateTime Now()
+        {
+            return new DateTime(DateTime.UtcNow.Ticks - InfluxDbEnvironment.EpochTicks);
+        }
+    }
+}
