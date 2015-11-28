52a8c9ae23cdef7ec1671e16e674d4789fc606da
Daniel Wertheim <daniel@wertheim.se>
2015-11-28 10:18:03 +0100

    Correct AssemblyInfo

--- src/projects/MyInfluxDbClient/Properties/AssemblyInfo.cs
+++ src/projects/MyInfluxDbClient/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("InfluxDbClient")]
+[assembly: AssemblyTitle("MyInfluxDbClient")]
 [assembly: ComVisible(false)]
 [assembly: Guid("8bdc0c80-d83b-42eb-915b-a1694559e16a")]
