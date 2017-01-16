1e7be5e67bb5e6b62e5decc8a5f6f67bcc5f304b
Daniel Wertheim <daniel@wertheim.se>
2017-01-16 19:55:18 +0100

    CakeBuild

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -11,9 +11,6 @@ using System.Reflection;
 
 [assembly: AssemblyDescription("MyInfluxDbClient - a simple async client for InfluxDb.")]
 [assembly: AssemblyCompany("Daniel Wertheim")]
-[assembly: AssemblyCopyright("Copyright © 2015 Daniel Wertheim")]
+[assembly: AssemblyCopyright("Copyright © 2017 Daniel Wertheim")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-[assembly: AssemblyVersion("0.0.0.*")]
-[assembly: AssemblyInformationalVersion("0.0.0.*")]
